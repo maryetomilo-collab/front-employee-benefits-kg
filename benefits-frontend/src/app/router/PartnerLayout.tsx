8ba7f0316c258199e5fb737862cedb76e50586c9
Meerim <etoochenmilo@gmail.com>
2026-08-09 12:37:17 +0600

    feat: add PartnerLayout Header

--- benefits-frontend/src/app/router/PartnerLayout.tsx
+++ benefits-frontend/src/app/router/PartnerLayout.tsx
@@ -1,17 +1,42 @@
-import { Layout, Typography, theme } from 'antd'
-import { Outlet } from 'react-router'
+import { Flex, Layout, Tag, Typography, theme } from "antd";
+import { Outlet, useParams } from "react-router";
+import { useGetPartner } from "../../api/endpoints/partners/partners";
 
-const { Content, Header } = Layout
+const { Content, Header } = Layout;
 
 export function PartnerLayout() {
-  const { token } = theme.useToken()
+  const { token } = theme.useToken();
+  const { partnerId = "" } = useParams();
+  const partnerQuery = useGetPartner(partnerId, {
+    query: { enabled: Boolean(partnerId) },
+  });
+  const partner = // TODO: Обработка ошибок
+    partnerQuery.data?.status === 200 ? partnerQuery.data.data : undefined;
 
   return (
-    <Layout style={{ minHeight: '100vh' }}>
-      <Header style={{ display: 'flex', alignItems: 'center' }}>
-        <Typography.Title level={4} style={{ margin: 0, color: token.colorTextLightSolid }}>
-          Employee Benefits
-        </Typography.Title>
+    <Layout style={{ minHeight: "100vh" }}>
+      <Header
+        style={{
+          display: "flex",
+          alignItems: "center",
+          background: token.colorBgContainer,
+        }}
+      >
+        {partner ? (
+          <Flex align="center" gap={token.marginLG}>
+            <Flex vertical align='start' gap={token.marginXXS} >
+              <Typography.Title level={4} style={{ margin: 0 }}>
+                {partner.name}
+              </Typography.Title>
+              <Typography.Text type="secondary">
+                ID партнёра: {partner.id}
+              </Typography.Text>
+            </Flex>
+            <Tag color="success" variant='outlined'>
+              АКТИВЕН
+            </Tag>
+          </Flex>
+        ) : null}
       </Header>
 
       <Content
@@ -24,7 +49,7 @@ export function PartnerLayout() {
         <Outlet />
       </Content>
     </Layout>
-  )
+  );
 }
 
-export default PartnerLayout
+export default PartnerLayout;
